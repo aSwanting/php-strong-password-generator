c6e173c0a5f030c727040fc66006363ba704a184
aSwanting <gab.damico@gmail.com>
2023-11-23 17:38:17 +0100

    add: bootstrap style

--- index.php
+++ index.php
@@ -14,13 +14,13 @@ include __DIR__ . "/partials/functions/functions.php";
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
-<body class="bg-dark bg-gradient bg-opacity-75 vh-100 overflow-hidden">
+<body class="bg-success bg-gradient bg-opacity-75 vh-100 overflow-hidden">
     <div class="container p-5 my-5 text-center w-75">
-        <h1 class="display-6 text-white text-uppercase mb-3">Password Generator</h1>
+        <h1 class="text-uppercase fw-semibold display-5 mb-3 text-white">Password Generator</h1>
         <div class="container bg-white border rounded rounded-5 p-5 shadow">
 
             <!-- Form Start -->
-            <form class="row g-3 mb-5 justify-content-center align-items-end" method="get">
+            <form class="row g-3 justify-content-center align-items-end" method="get">
 
                 <div class="col-auto">
                     <label class="form-label" for="password-length">Choose password length between 8 and 32 characters</label>
--- partials/server/password.php
+++ partials/server/password.php
@@ -1,10 +1,9 @@
+<!-- ////////////////////////////////////////// PHP ////////////////////////////////////////// -->
 <?php
-
 session_start();
 $password =  $_SESSION["password"];
-
 ?>
-
+<!-- ////////////////////////////////////////// HTML ////////////////////////////////////////// -->
 <!DOCTYPE html>
 <html lang="en">
 
@@ -15,14 +14,16 @@ $password =  $_SESSION["password"];
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
-<body class="bg-dark bg-gradient bg-opacity-75 vh-100 overflow-hidden">
+<body class="bg-success bg-gradient bg-opacity-75 vh-100 overflow-hidden">
+    <div class="container p-5 my-5 text-center w-75">
+        <h1 class="text-uppercase fw-semibold display-5 mb-3 text-white">Generated Password</h1>
+        <div class="container bg-white border rounded rounded-5 p-5 shadow">
 
-    <div class="container bg-white border rounded rounded-5 p-5 my-5 shadow text-center">
-        <h1 class="text-uppercase fw-semibold display-5 mb-5">Generated Password</h1>
-        <h2 class="display-6 mb-5"><?= $password ?></h2>
-        <p class="text-secondary">Contains <?= strlen($password) ?> characters</p>
+            <h2 class="display-6 mb-5"><?= $password ?></h2>
+            <p class="text-secondary">Contains <?= strlen($password) ?> characters</p>
+
+        </div>
     </div>
-
 </body>
 
 </html>
